5c9a36b5f97647db58b015b67d43d1ca5ac07cbb
Chris Hunt <chris.hunt@hudl.com>
2022-02-18 09:37:17 +0000

    Move types back to one file for better build

--- src/types/api.ts
+++
@@ -1,8 +0,0 @@
-import { Category, ClubDiscount, Location, Product } from ".";
-
-export interface CoreResponse {
-  discount: ClubDiscount[];
-  categories: Category[];
-  products: Product[];
-  locations: Location[];
-}
